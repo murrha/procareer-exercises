826301d7ddea54d774ba356fab128f1884279e0e
murrha <murrha@outlook.com>
2023-09-11 20:16:25 -0700

    changed function and html. Added css

--- Day14/script.js
+++ Day14/script.js
@@ -1,7 +1,7 @@
+
+
 async function getWeather(){
     let city = document.getElementById('city').value; 
-    //console.log(city);
-
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=93f26e3c57081a6210de53b8dcfdfea4`;
 
     let resp = await fetch(apiUrl);
@@ -18,19 +18,21 @@ async function getWeather(){
             alert("Unable to find city. Please try again.");
         }
     })
-    .then(displayData);
+    .then(displayWeather);
 }
 
-function displayData(data){
-    document.getElementById('weather').innerHTML = 
-    `
-    <img src = "http://openweathermap.org/img/w/${data.weather[0].icon}.png"/>
-    <p>Main: ${Math.floor((data.main.temp - 273.15) * 9/5 + 32)}°F</p>
-    <p>Min: ${Math.floor((data.main.temp_min - 273.15) * 9/5 + 32)}°F</p>
-    <p>Max: ${Math.floor((data.main.temp_max - 273.15) * 9/5 + 32)}°F</p>
-    <p>Weather: ${data.weather[0].main}</p>
-    <p>Description: ${data.weather[0].description}</p>
-    <p>${data.wind.speed}</p>
-    <p>Timezone: ${data.timezone}</p>
-    `;
+function displayWeather(data){
+
+    let image = document.querySelector("#weather-img");
+    let temperature = document.querySelector("#weather-temp"); 
+    let description = document.querySelector("#weather-description");
+    let humidity = document.querySelector("#weather-humidity");
+    let windSpeed = document.querySelector("#wind-speed");
+
+    image.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    temperature.innerHTML = `${Math.floor((data.main.temp - 273.15) * 9/5 + 32)}°F`;
+    description.innerHTML = `${data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.substring(1)}`; 
+    humidity.innerHTML = `${data.main.humidity}°F`; 
+    windSpeed.innerHTML = `${data.wind.speed} kmph`; 
+  
 }
