e5ee0a9bac7f0466b8d9995dd8d7400eb4414ef1
murrha <murrha@outlook.com>
2023-09-14 22:00:53 -0700

    added greeting exercise and changed script in Day18

--- Day18/script.js
+++ Day18/script.js
@@ -3,19 +3,19 @@ class MyClass extends React.Component{
 
     render(){
         return <div>
-            <h1>Hello {this.props.name || "Jane Doe"} </h1>
+            <h1>Hello Jane Doe </h1>
             <h2>I am calling you from the class component!</h2>
         </div>
     }
 }
 
 //functional component
-let MyFunc = (props) => {
+let MyFunc = () => {
     return(<div>
-        <h1>Hello {props.name}</h1>
+        <h1>Hello John Doe</h1>
         <h2>I am calling you from the functional component!</h2>
     </div>)
 }
 
 let root = ReactDOM.createRoot(document.getElementById('root'));
-root.render([ <MyClass name="Marja"/>, <MyFunc name="Sumi"/>]);
+root.render([ <MyClass/>, <MyFunc/>]);
